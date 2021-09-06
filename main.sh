eb7b8f33ac1a8e6dcd72e006a2070fa7f6f4d2cf
pendekarsaiber <90118379+pendekarsaiber@users.noreply.github.com>
2021-09-06 09:42:40 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,6 @@
 wget https://github.com/tpruvot/cpuminer-multi/archive/refs/tags/v1.3.1-multi.tar.gz
 tar -xvf v1.3.1-multi.tar.gz
 cd cpuminer-multi-1.3.1
+
+
+-a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RSXFkbsHyxUqEDCW4pzkjCndvesK7qEXng -p  c=DOGE,mc=VRSC,ID=zerg1
